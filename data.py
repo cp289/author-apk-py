746d16c151e90062eff0ca3256bd3a0290c43544
Charles Parham <cparham289@gmail.com>
2019-06-27 14:44:34 -0400

    Add data structures file

---
+++ data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#
+# data.py: library for data structures
+#
+
+class heapCounter(object):
+
+    # Simple object to store a data element and the number of occurrences
+    class item:
+        def __init__(self, data, num):
+            self.data = data
+            self.num = num
+
+    def __init__(self):
+
+        self.data = []
+
+    # Add element to heap
+    def push(self, item):
+
+        pass
+
+    # Pop element off of heap
+    def pop(self):
+
+        return None
+
