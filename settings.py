d16eda2982f670d55cbe0d260fe4f228cbca83b8
Charles Parham <cparham289@gmail.com>
2019-07-09 14:58:01 -0400

    Fix NO_FILTER

--- settings.py
+++ settings.py
@@ -4,10 +4,10 @@
 #
 
 # `x in NO_FILTER` will always return True
-class NO_FILTER(object):
-    __contains__ = lambda item: True
+class NO_FILTER:
+    __contains__ = lambda self, item: True
 
 VERBOSE = False             # Whether output is verbose
 DEBUG = False               # Whether to display debug output
-DEBUG_FILTER = NO_FILTER    # 'who' filters for debugging
+DEBUG_FILTER = NO_FILTER()  # 'who' filters for debugging
 
